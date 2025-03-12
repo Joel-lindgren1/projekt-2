412077c870a31ec12c16e7f53ea32d427961c5ee
joel-lindgren1 <joel.lindgren@elev.ga.ntig.se>
2025-03-12 16:06:34 +0100

    knappar funkar

--- script.js
+++ script.js
@@ -1,43 +1,24 @@
-let cherry1=document.querySelector("cherry1")
-let cherry10=document.querySelector("cherry10")
-let köpcherry=document.querySelector("köpcherry")
+let cart = {};
 
-let blueberry1=document.querySelector("blueberry1")
-let blueberry10=document.querySelector("blueberry10")
-let köplueberry=document.querySelector("köpblueberry")
 
-let citrus1=document.querySelector("citrus1")
-let citrus10=document.querySelector("citrus10")
-let köpcitrus=document.querySelector("köpcitrus")
+function addToCart(productName, quantity) {
+    if (cart[productName]) {
+        cart[productName] += quantity;
+    } else {
+        cart[productName] = quantity;
+    }
 
-let tropical1=document.querySelector("tropical1")
-let tropical10=document.querySelector("tropical10")
-let köptropical=document.querySelector("köptropical")
-
-const container = document.querySelector(".varukorg ul");
-
-function addItemToCart(itemName) {
-    const newElement = document.createElement("li");
-    newElement.textContent = itemName;
-    container.appendChild(newElement);
+    updateCartUI();
 }
 
 
-document.querySelector("#produkter").addEventListener("click", (event) => {
-    if (event.target.tagName === "BUTTON") {
-        const button = event.target;
-        const product = button.parentElement.querySelector("p").textContent;
+function updateCartUI() {
+    const cartList = document.querySelector("#cartList");
+    cartList.innerHTML = ''; 
 
-        if (button.textContent.includes("Köp")) {
-            addItemToCart(product);
-        }
+    for (let productName in cart) {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${cart[productName]}x ${productName}`;
+        cartList.appendChild(listItem);
     }
-});
-
-
-
-
-
-
-
-
+}
